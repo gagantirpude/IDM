cdba92ccb42c82c7cc771136f6f9caac209c0917
Gagan Tirpude <95214697+gagantirpude@users.noreply.github.com>
2024-01-01 11:49:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# IDM
+ IDM is Java base software. in this application i use JavaFX. with javaFX we can create software for windows
---
+++ src/main/java/org/example/config/AppConfig.java
@@ -0,0 +1,7 @@
+package org.example.config;
+
+public class AppConfig {
+    public static final String DOWNLOAD_PATH="W:\\Programmer\\Project\\Java\\download";
+    //other config information
+
+}
